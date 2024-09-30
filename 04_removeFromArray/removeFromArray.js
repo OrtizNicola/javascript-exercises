f4a84b6cd9fbc192528e0a65ca4aa553a6d714c1
OrtizNicola <130187770+OrtizNicola@users.noreply.github.com>
2024-09-30 16:38:39 -0500

    solve removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
+    let valsToRemove = Array.from(arguments).slice(1);
+    return arr.filter((x) => !valsToRemove.includes(x))
 };
 
 // Do not edit below this line
