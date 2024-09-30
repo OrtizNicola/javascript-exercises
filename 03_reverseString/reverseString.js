332582679d8ee1de13791afafe7e9a9c24150d26
OrtizNicola <130187770+OrtizNicola@users.noreply.github.com>
2024-09-30 15:58:04 -0500

    solve reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("");
+    let halfLen = Math.floor(arr.length / 2);
+    for(let i = 0; i < halfLen; i++) {
+        [arr[i], arr[arr.length - i - 1]] = [arr[arr.length - i - 1], arr[i]]
+    }
+    return arr.join("")
 };
 
 // Do not edit below this line
