87332335585aac013f2fa0fd8c3fc396e4c0bef5
OrtizNicola <130187770+OrtizNicola@users.noreply.github.com>
2024-09-30 15:45:42 -0500

    solve repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(substr, nTimes) {
+    if (nTimes < 0) {
+        return 'ERROR'
+    }
 
+    finalString = '';
+    for(let i = 0; i < nTimes; i++) {
+        finalString += substr;
+    }
+    return finalString
 };
 
 // Do not edit below this line
