12ca7fbbfc0316e7aabe9755b220de4bfea0b633
OrtizNicola <130187770+OrtizNicola@users.noreply.github.com>
2024-09-30 23:39:47 -0500

    solve calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, curr) => total + curr, 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, curr) => total * curr, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b)
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n < 0) {
+    return "ERROR"
+  } else if (n == 0) {
+    return 1
+  }
+
+	let ans = 1;
+  for (let i = 2; i <= n; i++) {
+    ans *= i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
