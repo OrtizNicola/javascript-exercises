4fbd7cd058733f68115a571f553980be2c87d11c
OrtizNicola <130187770+OrtizNicola@users.noreply.github.com>
2024-10-04 18:52:09 -0500

    solve fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n == 1 || n == 2) {
+        return 1
+    } else if (n == 0) {
+        return 0
+    } else if (n < 0) {
+        return "OOPS"
+    }
 
+    let prev = 1;
+    let nxt = 1;
+    let ans;
+    for (let i = 3; i <= n; i++) {
+        ans = prev + nxt;
+        prev = nxt;
+        nxt = ans;
+    }
+    return ans
 };
 
 // Do not edit below this line
