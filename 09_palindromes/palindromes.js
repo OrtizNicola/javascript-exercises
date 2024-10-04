c3adad96e2051b341927ea3d6ac74f91784add44
OrtizNicola <130187770+OrtizNicola@users.noreply.github.com>
2024-10-04 18:41:25 -0500

    solve palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let cleanString = str
+      .toLowerCase()
+      .split("")
+      .filter((char) => char.match(/^[0-9a-z]+$/))
+      .join("")
+    return cleanString.split("").reverse().join("") === cleanString
 };
 
 // Do not edit below this line
