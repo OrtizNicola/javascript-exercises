753d591cae3e69790e9abe895f34e6fa109c3f2d
OrtizNicola <130187770+OrtizNicola@users.noreply.github.com>
2024-10-04 19:16:12 -0500

    solve findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const currentYear = new Date().getFullYear();
+        const personAge = person.yearOfDeath ? 
+            person.yearOfDeath - person.yearOfBirth : 
+            currentYear - person.yearOfBirth;
+        
+        const oldestAge = oldest.yearOfDeath ? 
+            oldest.yearOfDeath - oldest.yearOfBirth : 
+            currentYear - oldest.yearOfBirth;
+        
+        return personAge > oldestAge ? person : oldest;
+    });
 };
 
 // Do not edit below this line
